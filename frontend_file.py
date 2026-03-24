93c51f3b6bbe595e1d7ffc4de004393e3826ec83
allauddin ali AHMAD <ahmad.aits@gmail.com>
2026-03-24 11:33:10 +0100

    initial commit- streamlit app

---
+++ frontend_file.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import requests
+
+ # Change this URL to the one of your API
+API_URL = "https://ml-reboot-app-964216878437.europe-west1.run.app/"
+
+st.title("Iris Predictor MVP")
+
+#st.write("Iris predictor")
+
+sepal_length = st.slider('Select a value for Sepal length', min_value=0, max_value=4, value=1, step=1)
+sepal_width = st.slider('Select a value for Sepal width',  min_value=0, max_value=4, value=1, step=1)
+petal_length = st.slider('Select a value for Petal length',  min_value=0, max_value=4, value=1, step=1)
+petal_width = st.slider('Select a value for Petal width',  min_value=0, max_value=4, value=1, step=1)
+
+url = f"{API_URL}/predict"
+params = {
+    'sepal_length': sepal_length,
+    'sepal_width': sepal_width,
+    'petal_length': petal_length,
+    'petal_width': petal_width,
+}
+
+response = requests.get(url, params=params).json()
+
+st.write(f"This flower belongs to category {str(response['prediction'])}")
